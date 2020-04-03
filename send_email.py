f9cb57518eeafacfc522af324021ec3909302a11
Jiajiabluegen <hejia.liu327@gmail.com>
2020-04-03 15:58:50 -0700

    first

---
+++ send_email.py
@@ -0,0 +1,36 @@
+import smtplib
+
+
+
+
+
+
+
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    user_account = 'jayzou2008@gmail.com'
+    password = ' '
+
+    server.login(user_account, password)
+
+    subject = 'Price'
+    body = 'check the link'
+
+    #msg = f"Subject: {subject}\n\n{body}"
+    msg = "Subject: %s\n\n%s" % (subject, body)
+    #msg = "check"
+
+    server.sendmail(
+        'jayzou2008@gmail.com',
+        'jay-zou2008@hotmail.com',
+        msg
+    )
+    print('Email has been sent')
+
+    server.quit()
+
+if (1):
+    send_email()
